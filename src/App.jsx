a611071f08008a0502c46ad6ee3d9b44c4f68d76
kSW <kij1285@gmail.com>
2026-02-20 12:54:32 +0900

    [FEAT] Route경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import InterviewSelect from "./pages/interview/InterviewSelect";
 import InterviewSession from "./pages/interview/InterviewSession";
-import InAnalysis from './pages/InAnalysis';
+import InAnalysis from "./pages/InAnalysis";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/interview/select" element={<InterviewSelect />} />
         <Route path="/interview/session" element={<InterviewSession />} />
+        <Route path="/interview/result" element={<InAnalysis />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
