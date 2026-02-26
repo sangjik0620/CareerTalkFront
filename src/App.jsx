e18663f438ffba0b913f3d24c82b520a72065c7b
JSY <jsyoo18@naver.com>
2026-02-26 15:53:23 +0900

    [FIX] 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // 페이지 및 컴포넌트 임포트
 import Home from "./pages/Home";
-import InAnalysis from "./pages/InAnalysis";
 import InterviewSession from "./pages/interview/InterviewSession";
 import InterviewSelect from "./pages/interview/InterviewSelect"; // 이름을 InterviewSelect로 수정
 import PortfolioResultPage from "./components/portfolio/PortfolioResultPage"; // 결과 페이지 임포트 추가
