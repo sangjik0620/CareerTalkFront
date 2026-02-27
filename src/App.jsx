f1b00503ebb48755e0c64e9893d33a438eb20751
JS <cshyun7575@naver.com>
2026-02-27 12:31:44 +0900

    [REFACTOR] analysis UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,24 @@ import InterviewSession from "./pages/interview/InterviewSession";
 import InterviewSelect from "./pages/interview/InterviewSelect"; // 이름을 InterviewSelect로 수정
 import PortfolioResultPage from "./components/portfolio/PortfolioResultPage"; // 결과 페이지 임포트 추가
 import Evaluation from "./pages/Evaluation";
+import CIAnalysis from "./pages/ClAnalysis";
+import CIAnalysisResult from "./pages/CIAnalysisResult";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* 자기소개서 분석 결과 */}
+        <Route
+          path="/analysis/result/:analysisId"
+          element={<CIAnalysisResult />}
+        />
+
         {/* 면접 관련 라우트 */}
         <Route path="/interview/select" element={<InterviewSelect />} />
         <Route path="/interview/session" element={<InterviewSession />} />
-        <Route path="/interview/result" element={<Evaluation/>} />
+        <Route path="/interview/result" element={<Evaluation />} />
 
         {/* 포트폴리오 관련 라우트 */}
         <Route path="/portfolio" element={<PfAnalysis />} />
