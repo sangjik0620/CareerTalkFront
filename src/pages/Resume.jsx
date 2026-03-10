f065444483a09a56842a4de3fc5394db00d2072c
JKM <jkm2771@gmail.com>
2026-03-10 12:16:05 +0900

    [FIX] 비 로그인 시 로그인 알림 창 문구 변경

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -48,7 +48,7 @@ export default function Resume({ isOpen, onClose, onAnalyzeSuccess }) {
     }else {
     const token = localStorage.getItem("token");
     if (!token) {
-      alert("로그인이 필요한 서비스입니다.");
+      alert("로그인이 필요한 서비스입니다. 로그인 페이지로 이동합니다.");
       onClose?.();
       navigate("/login");
     }
